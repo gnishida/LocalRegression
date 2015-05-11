2d752b71fcb9f6b59b6922083690999bb46172cc
gnishida <gnishida@purdue.edu>
2015-05-11 15:56:48 -0400

    update.

---
+++ LocalRegression.bat
@@ -0,0 +1,3 @@
+cd LocalRegression
+set PATH=%PATH%;..\opencv
+LocalRegression.exe
--- LocalRegression/main.cpp
+++ LocalRegression/main.cpp
@@ -5,18 +5,40 @@
 
 using namespace std;
 
-int main() {
-	cv::Mat_<double> X, Y;
-	ml::loadDataset("samplesX.txt", X);
-	ml::loadDataset("samplesY.txt", Y);
+int main(int argc,char *argv[]) {
+	if (argc < 3) {
+		cout << endl;
+		cout << "Usage: " << argv[0] << " <filename of X> <filename of Y>" << endl;
+		cout << endl;
 
-	LocalLinearRegression llr(X, Y, 250);
-
-	for (double x = 1700; x < 3800; x+=100) {
-		cv::Mat_<double> m = (cv::Mat_<double>(1, 1) << x);
-		cv::Mat_<double> y = llr.predict(m);
-		cout << y << endl;
+		return -1;
 	}
+
+	cv::Mat_<double> X, Y;
+	ml::loadDataset(argv[1], X);
+	ml::loadDataset(argv[2], Y);
+
+	cv::Mat_<double> normalizedX, meanX, stddevX;
+	ml::normalizeDataset(X, normalizedX, meanX, stddevX);
+	cv::Mat_<double> normalizedY, meanY, stddevY;
+	ml::normalizeDataset(Y, normalizedY, meanY, stddevY);
+
+	cv::Mat_<double> trainingX, testX;
+	ml::splitDataset(normalizedX, 0.9, trainingX, testX);
+	cv::Mat_<double> trainingY, testY;
+	ml::splitDataset(normalizedY, 0.9, trainingY, testY);
+
+
+	LocalLinearRegression llr(trainingX, trainingY, 0.2);
+
+	double rmse = 0.0;
+	for (int i = 0; i < testX.rows; ++i) {
+		cv::Mat_<double> normalized_y_hat = llr.predict(testX.row(i));
+		rmse += sqrt(ml::mat_sum(ml::mat_square(testY.row(i) - normalized_y_hat)));
+	}
+	rmse /= testX.rows;
+
+	cout << rmse << endl;
 
 	return 0;
 }
